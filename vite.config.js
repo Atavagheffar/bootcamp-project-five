28a6007c4a057bb1214dc07f8eb32eaa83df4106
Ata Ollah Vaghef Far <a.vagheffar@gmail.com>
2025-07-14 09:50:16 +0330

    fix base vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/bootcamp-project-five",
+  base: "/bootcamp-project-five/",
 });
